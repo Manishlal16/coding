dfe8a90ce9ecc19b183b287f59298b317f243435
Manishlal16 <manishlal005@gmail.com>
2022-09-23 15:01:52 +0530

    msg2

--- src/org/sample/BusinessTemplate.java
+++ src/org/sample/BusinessTemplate.java
@@ -4,6 +4,6 @@ public abstract class BusinessTemplate {
 public abstract void login();
 private void signup() {
 	System.out.println("account creation");
-	System.out.println("account");
+	System.out.println("account123");
 }
 }
